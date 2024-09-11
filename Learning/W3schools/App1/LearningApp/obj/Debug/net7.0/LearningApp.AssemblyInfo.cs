6abc24a0411236376064ce74177b2c9e37cd75c3
Justin Pope <pope2014@gamil.com>
2024-09-11 16:22:48 -0500

    update

--- Learning/W3schools/App1/LearningApp/obj/Debug/net7.0/LearningApp.AssemblyInfo.cs
+++ Learning/W3schools/App1/LearningApp/obj/Debug/net7.0/LearningApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1abbc6fe0c4169555efccf6b9f46e3dfd5fe18a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cb9cf3738ee65db547bd37187991b9987790b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
